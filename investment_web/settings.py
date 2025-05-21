765dec47f85efe06ee4698649a194f81944847e0
OJESH-DHK <ojesbirajandhakal@gmail.com>
2025-05-21 17:28:50 +0545

    “comment”

--- investment_web/settings.py
+++ investment_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ppk2+jn&jj@qnj(v=-u287_82b-gg%t)6(#++a90r_4db1&)fb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Media settings
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
